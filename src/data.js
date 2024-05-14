f3597a7ea59f74d7cd0a2d5b37e8a36e2b8daf07
01101010-01110000 <0110101001110000_WK@proton.me>
2024-05-14 10:11:50 -0300

    Test

--- src/data.js
+++ src/data.js
@@ -24,7 +24,7 @@ const data = {
       category: 'Ned Hermenn',
       questions: [
         {
-          question: 'Você gosta de pizza?',
+          question: 'test',
           options: ['Sim', 'Não'],
           answer: 'Sim',
         },
