55d62cdebf8514a44a8800718938ba8c2c6ab94e
felkip <149197855+felkip@users.noreply.github.com>
2024-05-14 12:23:22 -0300

    Atualizar o data.js

--- src/data.js
+++ src/data.js
@@ -6,239 +6,239 @@ const data = {
         {
           question: 'Escrevo listas daquilo que tenho de fazer?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],
           answer: ''
         },
         {
           question: 'Realizo coisas desde que as instruções escritas sejam bem detalhadas?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],
           answer: ''
         },
         {
           question: 'Gosto de fazer palavras cruzadas?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],
           answer: ''
         },
         {
           question: 'Gosto de exposições e museus?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],
           answer: ''
         },
         {
           question: 'Localizo-me com facilidade em uma cidade nova se tiver um mapa?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],
           answer: ''
         },
         {
           question: 'Todos os meses assisto a vários filmes?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],
           answer: ''
         },
         {
           question: 'Não tenho boa impressão de alguém se estiver mal vestido?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],
           answer: ''
         },
         {
           question: 'Gosto de observar as pessoas?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],
           answer: ''
         },
         {
           question: 'Acho que flores realmente embelezam a casa e o escritório?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],
           answer: ''
         },
         {
           question: 'Mando consertar os arranhões do meu carro o mais rápido possível?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],
           answer: ''
         },
         {
           question: 'Gosto de longas conversas?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],
           answer: ''
         },{
           question: 'Gosto de programas de entrevistas, no rádio e na TV?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],
           answer: ''
         },
         {
           question: 'Sou bom ouvinte?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],
           answer: ''
         },
         {
           question: 'Prefiro os noticiários de rádio e TV do que jornais e revistas?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],
           answer: ''
         },
         {
           question: 'Sinto-me mal quando meu carro faz algum barulho diferente?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],
           answer: ''
         },
         {
           question: 'Quando ouço música presto muita atenção à letra?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],,
           answer: ''
         },
         {
           question: 'Posso dizer muito sobre alguém somente pelo seu tom de voz?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],
           answer: ''
         },
         {
           question: 'Prefiro fazer uma palestra a escrever sobre um assunto?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],
           answer: ''
         },
         {
           question: 'As pessoas dizem que, às vezes, falo demais?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],
           answer: ''
         },
         {
           question: 'Costumo conversar comigo mesmo ou com meu cão ou gato?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],
           answer: ''
         },
         {
           question: 'Quando ouço música, não consigo deixar de mexer as mãos e os pés?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],,
           answer: ''
         },
         {
           question: 'Gosto muito de estar ao ar livre?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],
           answer: ''
         },
         {
           question:'Tenho boa coordenação motora?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],
           answer: ''
         },
         {
           question:'Tenho tendência a ganhar peso?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],
           answer: ''
         },
         {
           question:'Gosto de criar animais de estimação?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],
           answer: ''
         },
         {
           question:'Toco as pessoas quando converso?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],
           answer: ''
         },
         {
           question:'Gosto mais de praticar esportes do que assistir a eles?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],
           answer: ''
         },
         {
           question:'Adoro banho, piscina, sauna?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],
           answer: ''
         },
         {
           question:'Gosto de levantar-me e espreguiçar-me com frequência?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],
           answer: ''
         },
         {
           question:'Posso dizer muito sobre uma pessoa através de seu aperto de mão?',
           options:[
-            'Sim',
-            'Não'
+          'Sim',
+          'Não'
           ],
           answer: ''
         },
