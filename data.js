d2eb8316b2e67729a35061049e4b4760a24eabfb
felkip <149197855+felkip@users.noreply.github.com>
2024-05-14 11:31:21 -0300

    Add files via upload

--- data.js
+++ data.js
@@ -6,152 +6,152 @@ const data = {
         {
           question: 'Escrevo listas daquilo que tenho de fazer?',
           options:['Sim','Não'],
-          max_length:1
+          answer: ''
         },
         {
           question: 'Realizo coisas desde que as instruções escritas sejam bem detalhadas?',
           options:['Sim', 'Não'],
-          max_length:1
+          answer: ''
         },
         {
           question: 'Gosto de fazer palavras cruzadas?',
           options:['Sim', 'Não'],
-          max_length:1
+          answer: ''
         },
         {
           question: 'Gosto de exposições e museus?',
           options:['Sim', 'Não'],
-          max_length:1
+          answer: ''
         },
         {
           question: 'Localizo-me com facilidade em uma cidade nova se tiver um mapa?',
           options:['Sim', 'Não'],
-          max_length:1
+          answer: ''
         },
         {
           question: 'Todos os meses assisto a vários filmes?',
           options:['Sim', 'Não'],
-          max_length:1
+          answer: ''
         },
         {
           question: 'Não tenho boa impressão de alguém se estiver mal vestido?',
           options:['Sim', 'Não'],
-          max_length:1
+          answer: ''
         },
         {
           question: 'Gosto de observar as pessoas?',
           options:['Sim', 'Não'],
-          max_length:1
+          answer: ''
         },
         {
           question: 'Acho que flores realmente embelezam a casa e o escritório?',
           options:['Sim', 'Não'],
-          max_length:1
+          answer: ''
         },
         {
           question: 'Mando consertar os arranhões do meu carro o mais rápido possível?',
           options:['Sim', 'Não'],
-          max_length:1
+          answer: ''
         },
         {
           question: 'Gosto de longas conversas?',
           options:['Sim', 'Não'],
-          max_length:1
+          answer: ''
         },
         {
           question: 'Gosto de programas de entrevistas, no rádio e na TV?',
           options:['Sim', 'Não'],
-          max_length:1
+          answer: ''
         },
         {
           question: 'Sou bom ouvinte?',
           options:['Sim', 'Não'],
-          max_length:1
+          answer: ''
         },
         {
           question: 'Prefiro os noticiários de rádio e TV do que jornais e revistas?',
           options:['Sim', 'Não'],
-          max_length:1
+          answer: ''
         },
         {
           question: 'Sinto-me mal quando meu carro faz algum barulho diferente?',
           options:['Sim', 'Não'],
-          max_length:1
+          answer: ''
         },
         {
           question: 'Quando ouço música presto muita atenção à letra?',
           options:['Sim', 'Não'],
-          max_length:1
+          answer: ''
         },
         {
           question: 'Posso dizer muito sobre alguém somente pelo seu tom de voz?',
           options:['Sim', 'Não'],
-          max_length:1
+          answer: ''
         },
         {
           question: 'Prefiro fazer uma palestra a escrever sobre um assunto?',
           options:['Sim', 'Não'],
-          max_length:1
+          answer: ''
         },
         {
           question: 'As pessoas dizem que, às vezes, falo demais?',
           options:['Sim', 'Não'],
-          max_length:1
+          answer: ''
         },
         {
           question: 'Costumo conversar comigo mesmo ou com meu cão ou gato?',
           options:['Sim', 'Não'],
-          max_length:1
+          answer: ''
         },
         {
           question: 'Quando ouço música, não consigo deixar de mexer as mãos e os pés?',
           options:['Sim', 'Não'],
-          max_length:1
+          answer: ''
         },
         {
           question: 'Gosto muito de estar ao ar livre?',
           options:['Sim', 'Não'],
-          max_length:1
+          answer: ''
         },
         {
           question:'Tenho boa coordenação motora?',
           options:['Sim', 'Não'],
-          max_length:1
+          answer: ''
         },
         {
           question:'Tenho tendência a ganhar peso?',
           options:['Sim', 'Não'],
-          max_length:1
+          answer: ''
         },
         {
           question:'Gosto de criar animais de estimação?',
           options:['Sim', 'Não'],
-          max_length:1
+          answer: ''
         },
         {
           question:'Toco as pessoas quando converso?',
           options:['Sim', 'Não'],
-          max_length:1
+          answer: ''
         },
         {
           question:'Gosto mais de praticar esportes do que assistir a eles?',
           options:['Sim', 'Não'],
-          max_length:1
+          answer: ''
         },
         {
           question:'Adoro banho, piscina, sauna?',
           options:['Sim', 'Não'],
-          max_length:1
+          answer: ''
         },
         {
           question:'Gosto de levantar-me e espreguiçar-me com frequência?',
           options:['Sim', 'Não'],
-          max_length:1
+          answer: ''
         },
         {
           question:'Posso dizer muito sobre uma pessoa através de seu aperto de mão?',
           options:['Sim', 'Não'],
-          max_length:1
+          answer: ''
         },
       ],
     },
